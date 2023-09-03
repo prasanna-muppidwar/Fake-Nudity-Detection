58cdf191efb4951ef9cad4f23d9fdbbbfedf9f81
Prasanna Muppidwar <mprasanna5557@gmail.com>
2023-09-03 14:34:13 +0530

    Inputs Added Succesfully!

--- app.py
+++ app.py
@@ -4,53 +4,56 @@ import os
 import requests
 from dotenv import load_dotenv
 from flask import Flask, redirect, url_for, request, render_template, session
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+# Load environment variables from .env
 load_dotenv()
 
+# Define the directory where uploaded images will be stored
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Define allowed image file extensions (you can extend this list)
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'gif'}
+
+# Function to check if a filename has an allowed extension
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-
 @app.route('/', methods=['POST'])
 def index_post():
-    # Read the values from the form
+    # Check if a file was uploaded
+    if 'image' in request.files:
+        file = request.files['image']
+        if file.filename == '':
+            return render_template('index.html', error='No selected file')
+        
+        if file and allowed_file(file.filename):
+            # Save the uploaded file to the UPLOAD_FOLDER
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+            
+            # Perform image processing (replace this with your actual image processing logic)
+            # Here, we're assuming you're using a hypothetical image processing function
+            # called process_image that takes the filepath as input and returns some result.
+            image_processing_result = process_image(filepath)
+            
+            return render_template('results.html', image_processing_result=image_processing_result)
+        else:
+            return render_template('index.html', error='Invalid file format')
+
+    # If no file was uploaded, continue with text translation as before
     original_text = request.form['text']
     target_language = request.form['language']
-
-    # Load the values from .env
-    key = os.environ['KEY']
-    endpoint = os.environ['ENDPOINT']
-    location = os.environ['LOCATION']
-
-    # Indicate that we want to translate and the API version (3.0) and the target language
-    path = '/translate?api-version=3.0'
-    # Add the target language parameter
-    target_language_parameter = '&to=' + target_language
-    # Create the full URL
-    constructed_url = endpoint + path + target_language_parameter
-
-    # Set up the header information, which includes our subscription key
-    headers = {
-        'Ocp-Apim-Subscription-Key': key,
-        'Ocp-Apim-Subscription-Region': location,
-        'Content-type': 'application/json',
-        'X-ClientTraceId': str(uuid.uuid4())
-    }
-
-    # Create the body of the request with the text to be translated
-    body = [{'text': original_text}]
-
-    # Make the call using post
-    translator_request = requests.post(
-        constructed_url, headers=headers, json=body)
-    # Retrieve the JSON response
-    translator_response = translator_request.json()
-    # Retrieve the translation
-    translated_text = translator_response[0]['translations'][0]['text']
+    
+    # ... (the rest of your translation logic)
 
     return render_template(
         'results.html',
@@ -58,3 +61,6 @@ def index_post():
         original_text=original_text,
         target_language=target_language
     )
+
+if __name__ == '__main__':
+    app.run(debug=True)
