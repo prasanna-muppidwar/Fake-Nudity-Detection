7e939cc82db767b05edf01ad0252744fdd7bd496
Aayush <aayush.paigwar123@gmail.com>
2023-09-03 20:34:04 +0530

    URL updated

--- app.py
+++ app.py
@@ -1,73 +1,95 @@
 import os
+from tabnanny import filename_only
 import requests
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-DEEPAI_API_KEY = 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K'
+DEEPAI_API_KEY = "656aabe8-347c-4db8-9f18-01d29d91de43"
 
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'gif'}
+ALLOWED_EXTENSIONS = {"jpg", "jpeg", "png", "gif"}
+
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        method = request.form['language']
-        if method == 'url-search':
-            image_url = request.form['text']
+    if request.method == "POST":
+        method = request.form["method"]
+        if method == "url-search":
+            image_url = request.form["text"]
             is_nsfw, nsfw_score, image_path = detect_nsfw_image_url(image_url)
-            return render_template('results.html', is_nsfw=is_nsfw, nsfw_score=nsfw_score, image_path=image_path)
+            return render_template(
+                "results.html",
+                is_nsfw=is_nsfw,
+                nsfw_score=nsfw_score,
+                image_path=image_path,
+            )
 
-        elif method == 'upload-search':
-            if 'image' in request.files:
-                file = request.files['image']
-                if file.filename == '':
-                    return render_template('index.html', error='No selected file')
+        elif method == "upload-search":
+            if "image" in request.files:
+                file = request.files["image"]
+                if file.filename == " ":
+                    return render_template("index.html", error="No selected file")
 
                 if not allowed_file(file.filename):
-                    return render_template('index.html', error='Invalid file format')
+                    return render_template("index.html", error="Invalid file format")
 
                 filename = secure_filename(file.filename)
-                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
                 file.save(filepath)
 
                 is_nsfw, nsfw_score, image_path = detect_nsfw_image_file(filepath)
+                return render_template(
+                    "results.html",
+                    is_nsfw=is_nsfw,
+                    nsfw_score=nsfw_score,
+                    image_path=image_path,
+                )
 
-                # Pass the NSFW detection results and image path to the results template
-                return render_template('results.html', is_nsfw=is_nsfw, nsfw_score=nsfw_score, image_path=image_path)
+        elif method == "key-search":
+            original_text = request.form["text"]
+            target_language = request.form[
+                "target_language"
+            ]  # Replace with your translation logic
 
-        elif method == 'key-search':
-            original_text = request.form['text']
-            target_language = request.form['target_language']  # Replace with your translation logic
-            return render_template('results.html', original_text=original_text, target_language=target_language)
+            return render_template(
+                "results.html",
+                original_text=original_text,
+                target_language=target_language,
+            )
 
-    return render_template('index.html')
+    return render_template("index.html")
 
-def detect_nsfw_image_file(filepath):
+
+def detect_nsfw_image_file(image_path):
     api_url = "https://api.deepai.org/api/nsfw-detector"
     headers = {
-        'api-key': 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K',
+        "api-key": "656aabe8-347c-4db8-9f18-01d29d91de43",
     }
 
     try:
-        with open(filepath, 'rb') as image_file:
-            files = {'image': (os.path.basename(filepath), image_file)}
+        with open(image_path, "rb") as image_file:
+            files = {"image": (os.path.basename(image_path), image_file)}
             response = requests.post(api_url, files=files, headers=headers)
             result = response.json()
-            nsfw_score = result.get('output', {}).get('nsfw_score', None)
+            nsfw_score = result.get("output", {}).get("nsfw_score", None)
 
             if nsfw_score is not None:
                 threshold = 0.5  # Adjust this threshold as needed
                 is_nsfw = nsfw_score >= threshold
+                image_path = None  # Initialize image_path as None
 
-                # Provide the correct image path based on the file saved
-                image_path = '/uploads/' + secure_filename(image_file.filename)
+                if is_nsfw:
+                    image_path = (
+                        "/static/uploads/" + filename_only
+                    )  # Change the path as needed
 
                 return is_nsfw, nsfw_score, image_path
             else:
@@ -77,18 +99,21 @@ def detect_nsfw_image_file(filepath):
         print(str(e))
         return False, None, None
 
+
 def detect_nsfw_image_url(image_url):
+    # Call the DeepAI NSFW Detector API to scan the image URL
     api_url = "https://api.deepai.org/api/nsfw-detector"
     headers = {
-        'api-key': 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K',
+        "api-key": "656aabe8-347c-4db8-9f18-01d29d91de43",
     }
 
-    data = {'image': image_url}
+    data = {"image": image_url}
 
     try:
         response = requests.post(api_url, data=data, headers=headers)
         result = response.json()
-        nsfw_score = result.get('output', {}).get('nsfw_score', None)
+        # Extract the NSFW score
+        nsfw_score = result.get("output", {}).get("nsfw_score", None)
 
         if nsfw_score is not None:
             threshold = 0.5  # Adjust this threshold as needed
@@ -96,7 +121,9 @@ def detect_nsfw_image_url(image_url):
             image_path = None  # Initialize image_path as None
 
             if is_nsfw:
-                image_path = image_url  # You can provide the URL itself as the image path
+                image_path = (
+                    image_url  # You can provide the URL itself as the image path
+                )
 
             return is_nsfw, nsfw_score, image_path
         else:
@@ -106,32 +133,10 @@ def detect_nsfw_image_url(image_url):
         print(str(e))
         return False, None, image_url
 
-# Serve uploaded images
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-@app.route('/results', methods=['POST'])
-def show_results():
-    method = request.form['language']
-    if method == 'upload-search':
-        if 'image' in request.files:
-            file = request.files['image']
-            if file.filename == '':
-                return render_template('index.html', error='No selected file')
 
-            if not allowed_file(file.filename):
-                return render_template('index.html', error='Invalid file format')
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
 
-            filename = secure_filename(file.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
-
-            is_nsfw, nsfw_score, image_path = detect_nsfw_image_file(filepath)
-
-            # Pass the NSFW detection results and image path to the results template
-            return render_template('results.html', is_nsfw=is_nsfw, nsfw_score=nsfw_score, image_path=image_path)
-
-if __name__ == '__main__':
-    app.run(debug=True)
-'''{'id': '579733d4-0da9-4fff-9387-451d02109b73', 'output': {'detections': [{'confidence': '0.86', 'bounding_box': [1806.5749999999998, 1216.2374999999997, 842.8312499999998, 487.20624999999995], 'name': 'Buttocks - Exposed'}, {'confidence': '0.65', 'bounding_box': [1095.3249999999998, 544.1062499999999, 462.31249999999994, 526.3249999999999], 'name': 'Female Breast - Covered'}], 'nsfw_score': 0.5617509484291077}}
-'''
+# Output
+"""{'id': '579733d4-0da9-4fff-9387-451d02109b73', 'output': {'detections': [{'confidence': '0.86', 'bounding_box': [1806.5749999999998, 1216.2374999999997, 842.8312499999998, 487.20624999999995], 'name': 'Buttocks - Exposed'}, {'confidence': '0.65', 'bounding_box': [1095.3249999999998, 544.1062499999999, 462.31249999999994, 526.3249999999999], 'name': 'Female Breast - Covered'}], 'nsfw_score': 0.5617509484291077}}
+"""
